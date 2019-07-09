2615262f8e4b250920a7200e12fbce1d1faf84e9
tssqzr <1214661466@qq.com>
2019-07-09 15:20:56 +0800

    新增返回为空的逻辑判断

--- Comm/Api.cs
+++ Comm/Api.cs
@@ -2,6 +2,7 @@
 using NEL.NNS.lib;
 using NEL_Swap_API.Service;
 using Newtonsoft.Json.Linq;
+using System;
 
 namespace NEL.Comm
 {
@@ -35,26 +36,45 @@ namespace NEL.Comm
         }
         public object getRes(JsonRPCrequest req, string reqAddr)
         {
-            JArray result = new JArray();
-            switch (req.method)
+            JArray result = null;
+            try
             {
-                // 获取GAS/USD价格
-                case "getExchangePrice":
-                    result = exchangeService.getExchangePrice();
-                    break ;
-                // 获取USD/CNY汇率
-                case "getExchangeRate":
-                    result = exchangeService.getExchangeRate();
-                    break;
-                // 获取资产列表
-                case "getAssetList":
-                    result = exchangeService.getAssetList(req.@params[0].ToString(), int.Parse(req.@params[1].ToString()), int.Parse(req.@params[2].ToString()));
-                    break;
-                case "getnodetype":
-                    result = new JArray { new JObject { { "nodeType", netnode } } };
-                    break;
+                switch (req.method)
+                {
+                    // 获取GAS/USD价格
+                    case "getExchangePrice":
+                        result = exchangeService.getExchangePrice();
+                        break;
+                    // 获取USD/CNY汇率
+                    case "getExchangeRate":
+                        result = exchangeService.getExchangeRate();
+                        break;
+                    // 获取资产列表
+                    case "getAssetList":
+                        result = exchangeService.getAssetList(req.@params[0].ToString(), int.Parse(req.@params[1].ToString()), int.Parse(req.@params[2].ToString()));
+                        break;
+                    case "getnodetype":
+                        result = new JArray { new JObject { { "nodeType", netnode } } };
+                        break;
+                }
+                //
+                if (result == null || result.Count == 0)
+                {
+                    JsonPRCresponse_Error resE = new JsonPRCresponse_Error(req.id, -1, "No Data", "Data does not exist");
+                    return resE;
+                }
             }
-            return result;
+            catch (Exception e)
+            {
+                JsonPRCresponse_Error resE = new JsonPRCresponse_Error(req.id, -100, "Parameter Error", e.Message);
+                return resE;
+            }
+
+            JsonPRCresponse res = new JsonPRCresponse();
+            res.jsonrpc = req.jsonrpc;
+            res.id = req.id;
+            res.result = result;
+            return res;
         }
     }
 }
